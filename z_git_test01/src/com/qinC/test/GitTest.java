718bb5bd3769215b6eead4514ba1f60fe4115ce3
SQL-JQC <2741118571@qq.com>
2020-10-18 00:12:53 +0800

    第一次提交idea中的Java项目......

---
+++ z_git_test01/src/com/qinC/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.qinC.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        String str = "Not Hello World!";
+
+        System.out.println(str);
+    }
+
+}
